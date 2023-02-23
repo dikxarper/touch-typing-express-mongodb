01acf8c48c730b854b9a43e47d3251a85abcd0dc
dikxarper <diasmerekeev1@gmail.com>
2023-02-23 08:10:45 +0600

    changes

--- models/stats.js
+++ models/stats.js
@@ -1,1 +1,49 @@
-const mongoose = require('mongoose')
+const { ObjectID } = require("bson")
+const mongoose = require("mongoose")
+
+const statSchema = new mongoose.Schema({
+  user_id: {
+    type: ObjectID,
+  },
+  text_id: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  wpm: {
+    type: Number,
+    required: true,
+  },
+  raw: {
+    type: Number,
+    required: true,
+  },
+  word_count: {
+    type: Number,
+  },
+  time: {
+    type: Number,
+  },
+  accuracy: {
+    type: Number,
+  },
+  consistency: {
+    type: Number,
+  },
+  correct: {
+    type: Number,
+    required: true,
+  },
+  incorrect: {
+    type: Number,
+    required: true,
+  },
+})
+
+const userDB = mongoose.connection.useDb("projDB")
+const Stat = userDB.model("stat", statSchema)
+
+module.exports = Stat
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,8 +1,28 @@
 const express = require("express")
 const router = express.Router()
+const Stat = require("../models/stats")
 
 router.get("/", (req, res) => {
   res.render("index")
 })
 
+router.post("/", (req, res) => {
+  const timeTag = document.querySelector(".time span b"),
+    wordTag = document.querySelector(".word span"),
+    mistakeTag = document.querySelector(".mistake span"),
+    wpmTag = document.querySelector(".wpm span"),
+    cpmTag = document.querySelector(".cpm span")
+
+  let cpm = cpmTag.innerText
+  let wpm = wpmTag.innerText
+  let raw = wpm + 20
+  let newStat = new Stat({
+    raw: raw,
+    wpm: wpm,
+    correct: 100,
+    incorrect: cpm,
+  })
+
+  newStat.save()
+})
 module.exports = router
